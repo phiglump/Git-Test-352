59a1a13d31e7717139078d628711baa31bb3e463
Philippe Lumpkin <phiglump@ut.utm.edu>
2019-01-15 15:04:16 -0600

    This is the new code for the guessing number program.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,48 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello, World!");
+            int userInput;
+            int counter = 1;
+
+            Console.WriteLine("I am thinking of a number from 0 to 100.");
+            Console.WriteLine("Take a guess at what the number is.");
+
+            Random random = new Random();
+            int randomNum = random.Next(0, 100);
+
+            userInput = Convert.ToInt32(Console.ReadLine());
+            bool done = false;
+            while (done == false)
+            {
+                if (randomNum == userInput)
+                {
+                    Console.WriteLine("You Guessed It! Great Job!");
+                    Console.WriteLine("The number was " + randomNum);
+                    Console.WriteLine("You only took " + counter + " Time(s).");
+                    done = true;
+                }
+                else if (randomNum < userInput)
+                {
+                    Console.WriteLine("Your guess was too high.");
+                    Console.WriteLine("Try again.");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                    ++counter;
+                }
+                else if (randomNum > userInput)
+                {
+                    Console.WriteLine("Your guess was too low.");
+                    Console.WriteLine("Try again.");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                    ++counter;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry that is the wrong number.");
+                    Console.WriteLine("Try again.");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                    ++counter;
+                }
+            }
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
