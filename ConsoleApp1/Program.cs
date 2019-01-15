65f5733431e59a40791f642083b73cb4738bb644
Philippe Lumpkin <phiglump@ut.utm.edu>
2019-01-15 14:23:38 -0600

    First Hello World program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello, World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
